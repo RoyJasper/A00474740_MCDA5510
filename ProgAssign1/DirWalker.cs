5f4bd2f8b05cf63c9e4036a6892ef2cb73401f82
A00474740 <Roy.Simon@smu.ca>
2023-10-08 22:31:07 -0300

    Signed-off-by: A00474740 <Roy.Simon@smu.ca>

---
+++ ProgAssign1/DirWalker.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace Assignment1
+{
+    public class DirWalker
+    {
+        public static void Main(String[] args)
+        {
+            DirWalker fm = new DirWalker();
+            fm.walk(@"..\..\..\Sample Data");
+        }
+
+        public void walk(String path)
+        {
+            try
+            {
+                string[] list = Directory.GetDirectories(path);
+                Console.WriteLine("\n -------------------------------- \nPassed Path " + path);
+                if (list == null) return;
+
+                foreach (string dirpath in list)
+                {
+                    if (Directory.Exists(dirpath))
+                    {
+                        walk(dirpath);
+                        Console.WriteLine("Dir:" + dirpath);
+                    }
+                }
+                 string[] fileList = Directory.GetFiles(path);
+                 foreach (string filepath in fileList)
+                 {
+
+                     Console.WriteLine("File:" + filepath);
+                 } 
+            }catch (Exception ex) { Console.WriteLine(ex.ToString()); }
+        }
+    }
+}
