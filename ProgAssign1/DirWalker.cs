1b7ab1a689df00cd12e72d1bce7ff27ac4adf167
A00474740 <Roy.Simon@smu.ca>
2023-10-09 17:33:02 -0300

    Identified all bad records and for each files.

--- ProgAssign1/DirWalker.cs
+++ ProgAssign1/DirWalker.cs
@@ -9,6 +9,8 @@ using System.Text.RegularExpressions;
 using System.Globalization;
 using System.Security.Cryptography.X509Certificates;
 using System.Reflection.Metadata;
+using System.Formats.Asn1;
+using LINQtoCSV;
 
 namespace Assignment1
 {
@@ -19,7 +21,7 @@ namespace Assignment1
         [Name("Last Name")]
         public string Last_Name { get; set; }
         [Name("Street Number")]
-        public int Street_Number { get; set; }
+        public string Street_Number { get; set; }
         [Name("Street")]
         public string Street_Name { get; set; }
         [Name("City")]
@@ -34,19 +36,31 @@ namespace Assignment1
         public string Phone { get; set; }
         [Name("email Address")]
         public string Email { get; set; }
+
+       
     }
     public class DirWalker
     {
+        public CsvWriter csvwriter ;
+        private object output;
 
         public static void Main(String[] args)
         {
             DirWalker fm = new DirWalker();
             fm.walk(@"..\..\..\Sample Data");
-            string csvPath = @"..\..\..\Output";
-            StreamWriter writter = new StreamWriter(csvPath);
-            CsvWriter csvwriter = new CsvWriter(writter, CultureInfo.InvariantCulture);
+            string csvPath = @"..\..\..\Output\FullData.csv";
+            if (File.Exists(csvPath))
+            {
+                File.Delete(csvPath);
+               // Console.WriteLine("File newely created");
+            }
+            FileStream fs = File.Create(csvPath);
+            fs.Close();
+           StreamWriter writter = new StreamWriter(csvPath);
+           CsvWriter csvwriter = new CsvWriter(writter, CultureInfo.InvariantCulture);
+            
 
-         }
+        }
 
         public void walk(String path)
         {
@@ -83,17 +97,73 @@ namespace Assignment1
 
         public void ReadCSV(string filepath)
         {
+            int status;
             var reader = new StreamReader(filepath);
             var csvreader = new CsvReader(reader, CultureInfo.InvariantCulture);
             var output = csvreader.GetRecords<Customer>().ToList();
+            //Console.WriteLine(output.ToArray());
+            foreach (var cus in output)
+            {
+                //Console.WriteLine(cus.Email, cus.Phone);
+                status = RecordChecker(cus);
+                if(status == 0)
+                {
+                    //Console.WriteLine("Good record :");
+                   // displayData(cus);
+                }
+                else if (status == 1)
+                {
+                    Console.WriteLine("Bad record ");
+                    displayData(cus);
+                }
+            }
+            //***Data Check
+
             List<String> strings = new List<String>(filepath.Split(@"\"));
-            Console.WriteLine(strings[strings.Count - 1]+" Has "+ output.Count);
+           // Console.WriteLine(strings[strings.Count-1]+" Has "+ output.Count);
             Console.WriteLine("");
+          // csvwriter.WriteRecords(output);
+        }
+
+        private void displayData(Customer cus)
+        {
+            Console.WriteLine("\n First_Name:{0} \n Last_Name:{1} \n Street_Number:{2} \n Street_Name:{3} \n City:{4} \n Province:{5} " +
+                "\n Poastal_Code:{6} " +
+                "\n Country:{7} \n Phone:{8} \n Email:{9} ", 
+                cus.First_Name , cus.Last_Name,cus.Street_Number,cus.Street_Name,cus.City,cus.Province,cus.Poastal_Code, cus.Country,
+                cus.Phone,cus.Email);
+           
         }
 
         public void WriteCSV(string filepath)
         {
+            //  csvwriter.WriteField()
+            var csvFileDescription = new CsvFileDescription
+            {
+                FirstLineHasColumnNames = true,
+                SeparatorChar = ','
+            };
+           // var csvContext = new CsvHelper.CsvContext();
+         //   csvContext.Write(output,filepath,csvFileDescription);
+        }
 
+        public static int RecordChecker(Customer cus)
+        {
+            // return 1;
+            //First Name	Last Name	Street Number	Street	City	Province	Postal Code	Country	Phone Number	email Address
+            string strRegex = @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z";
+            Regex re = new Regex(strRegex, RegexOptions.IgnoreCase);
+            if (cus.First_Name == "" || cus.Street_Number == "" || cus.Street_Name == "" || cus.City == "" || cus.Province == ""
+                || cus.Poastal_Code == "" || cus.Country == "" || cus.Phone == "" || cus.Email == "")
+                return 1;
+              else
+                 {  
+               if (re.IsMatch(cus.Email))
+                    return 0;
+                else
+                   return 1;
+                 }
+              // return 0;
         }
     }
 }
