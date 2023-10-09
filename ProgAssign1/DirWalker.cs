87d19bd4d7f8a6101eca0945fb5378af07c1048e
A00474740 <Roy.Simon@smu.ca>
2023-10-09 01:01:47 -0300

    Created Class

--- ProgAssign1/DirWalker.cs
+++ ProgAssign1/DirWalker.cs
@@ -2,9 +2,24 @@
 using CsvHelper;
 using System.IO;
 using System.Text.RegularExpressions;
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Assignment1
 {
+    public class Customer
+    {
+        public string First_Name { get; set; }
+        public string Last_Name { get; set; }
+        public int Street_Number { get; set; }
+        public string Street_Name { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public string Poastal_Code { get; set; }
+        public string Country { get; set; }
+        public int Phone { get; set; }
+        public string Email { get; set; }
+    }
     public class DirWalker
     {
         public static void Main(String[] args)
@@ -29,16 +44,29 @@ namespace Assignment1
                         Console.WriteLine("Dir:" + dirpath);
                     }
                 }
-                 string[] fileList = Directory.GetFiles(path);
-                 foreach (string filepath in fileList)
-                 {
+                string[] fileList = Directory.GetFiles(path);
+                foreach (string filepath in fileList)
+                {
                     // string strRegex = @"*.csv";
                     // Regex re = new Regex(strRegex, RegexOptions.IgnoreCase);
                     // if (re.IsMatch(filepath))
-                    if (filepath.Contains(".csv"))
+                    if (filepath.EndsWith(".csv"))
+                    {
                         Console.WriteLine("File:" + filepath);
-                 } 
-            }catch (Exception ex) { Console.WriteLine(ex.ToString()); }
+                        ReadCSV(filepath);
+                    }
+                }
+            }
+            catch (Exception ex) { Console.WriteLine(ex.ToString()); }
+        }
+
+        private void ReadCSV(string filepath)
+        {
+            var reader = new StreamReader(filepath);
+            var csvreader = new CsvReader(reader, CultureInfo.InvariantCulture);
+            var output = csvreader.GetRecords<dynamic>().ToList();
+            Console.WriteLine("");
+
         }
     }
 }
